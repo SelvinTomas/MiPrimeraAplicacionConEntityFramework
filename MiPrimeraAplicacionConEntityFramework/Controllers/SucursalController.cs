fc309c7090b6c322e2f744bf52a26c793f139271
Selvin Tomás <tomas.a12108@gmail.com>
2022-08-30 12:45:18 -0600

    Añadiendo segunda clase y modelo

---
+++ MiPrimeraAplicacionConEntityFramework/Controllers/SucursalController.cs
@@ -0,0 +1,31 @@
+﻿using MiPrimeraAplicacionConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionConEntityFramework.Controllers
+{
+    public class SucursalController : Controller
+    {
+        // GET: Sucursal
+        public ActionResult Index()
+        {
+            List<SucursalCLS> listaSucursal = null;
+            using (var db = new BDPasajeEntities())
+            {
+                listaSucursal = (from sucursal in db.Sucursal
+                                 where sucursal.BHABILITADO == 1
+                                 select new SucursalCLS
+                                 {
+                                     iidsucursal = sucursal.IIDSUCURSAL,
+                                     nombre = sucursal.NOMBRE,
+                                     telefono = sucursal.TELEFONO,
+                                     email = sucursal.EMAIL
+                                 }).ToList();
+            }
+            return View(listaSucursal);
+        }
+    }
+}
---
+++ MiPrimeraAplicacionConEntityFramework/Models/SucursalCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionConEntityFramework.Models
+{
+    public class SucursalCLS
+    {
+        [Display(Name = "ID")]
+        public int iidsucursal { get; set; }
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+        [Display(Name = "Dirección")]
+        public string direccion { get; set; }
+        [Display(Name = "Teléfono")]
+        public string telefono { get; set; }
+        [Display(Name = "E-mail")]
+        public string email { get; set; }
+        public DateTime fechaapertura { get; set; }
+        public int bhabilitado { get; set; }
+    }
+}

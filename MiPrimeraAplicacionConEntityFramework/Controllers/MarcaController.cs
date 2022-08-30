d4d2e259436efdfe50cae788fd98870efa42f718
Selvin Tomás <tomas.a12108@gmail.com>
2022-08-30 12:33:56 -0600

    Creando Modelo y añadiendo Bootstrap

---
+++ MiPrimeraAplicacionConEntityFramework/Controllers/MarcaController.cs
@@ -0,0 +1,31 @@
+﻿using MiPrimeraAplicacionConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionConEntityFramework.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            List<MarcaCLS> listaMarca = null;
+            using (var db = new BDPasajeEntities())
+            {
+                listaMarca = (from marca in db.Marca
+                              where marca.BHABILITADO == 1
+                              select new MarcaCLS
+                              {
+                                  iidmarca = marca.IIDMARCA,
+                                  nombre = marca.NOMBRE,
+                                  descripcion = marca.DESCRIPCION
+
+                              }).ToList();
+            }
+            return View(listaMarca);
+        }
+    }
+}
---
+++ MiPrimeraAplicacionConEntityFramework/Models/MarcaCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionConEntityFramework.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name = "Id")]
+        public int iidmarca { get; set; }
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+        public int bhabilitado { get; set; }
+    }
+}

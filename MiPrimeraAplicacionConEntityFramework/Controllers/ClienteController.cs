9f0bcc4df1be31b05e1171d47ee93e3dab6943d6
Selvin Tomás <tomas.a12108@gmail.com>
2022-08-31 11:34:39 -0600

    Creando modelo y vista cliente

---
+++ MiPrimeraAplicacionConEntityFramework/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using MiPrimeraAplicacionConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionConEntityFramework.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteCLS> listaCliente = null;
+            using (var db = new BDPasajeEntities())
+            {
+                listaCliente = (from cliente in db.Cliente
+                                where cliente.BHABILITADO == 1
+                                select new ClienteCLS
+                                {
+                                    iidcliente = cliente.IIDCLIENTE,
+                                    nombre = cliente.NOMBRE,
+                                    appaterno = cliente.APPATERNO,
+                                    apmaterno = cliente.APMATERNO,
+                                    telefonocelular = cliente.TELEFONOCELULAR
+                                }
+                                ).ToList();
+            }
+            return View(listaCliente);
+        }
+    }
+}
---
+++ MiPrimeraAplicacionConEntityFramework/Models/ClienteCLS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionConEntityFramework.Models
+{
+    public class ClienteCLS
+    {
+        [Display(Name = "ID")]
+        public int iidcliente { get; set; }
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+        [Display(Name = "Primer Apellido")]
+        public string appaterno { get; set; }
+        [Display(Name = "Segundo Apellido")]
+    
+        public string apmaterno { get; set; }
+        public string email { get; set; }
+        public string direccion { get; set; }
+        public int iidsexo { get; set; }
+        [Display(Name = "Teléfono Fijo")]
+        public string telefonofijo { get; set; }
+        public string telefonocelular { get; set; }
+        public int bhabilitado { get; set; }
+
+
+    }
+}

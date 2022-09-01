1a0c92928b43a700ef8252fb20c9638efc4976f9
Selvin Tomás <tomas.a12108@gmail.com>
2022-09-01 10:58:33 -0600

    Añadiendo formulario Agregar en Sucursal

--- MiPrimeraAplicacionConEntityFramework/Controllers/SucursalController.cs
+++ MiPrimeraAplicacionConEntityFramework/Controllers/SucursalController.cs
@@ -27,5 +27,31 @@ namespace MiPrimeraAplicacionConEntityFramework.Controllers
             }
             return View(listaSucursal);
         }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(SucursalCLS oSucursalCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oSucursalCLS);
+            }
+            using (var db = new BDPasajeEntities())
+            {
+                Sucursal oSucursal = new Sucursal();
+                oSucursal.NOMBRE = oSucursalCLS.nombre;
+                oSucursal.DIRECCION = oSucursalCLS.direccion;
+                oSucursal.TELEFONO = oSucursalCLS.telefono;
+                oSucursal.EMAIL = oSucursalCLS.email;
+                oSucursal.FECHAAPERTURA = oSucursalCLS.fechaApertura;
+                oSucursal.BHABILITADO = 1;
+                db.Sucursal.Add(oSucursal);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+            
+        }
     }
 }
--- MiPrimeraAplicacionConEntityFramework/Models/SucursalCLS.cs
+++ MiPrimeraAplicacionConEntityFramework/Models/SucursalCLS.cs
@@ -11,14 +11,28 @@ namespace MiPrimeraAplicacionConEntityFramework.Models
         [Display(Name = "ID")]
         public int iidsucursal { get; set; }
         [Display(Name = "Nombre")]
+        [StringLength(100, ErrorMessage = "Longitud máxima 100")]
+        [Required]
         public string nombre { get; set; }
         [Display(Name = "Dirección")]
+        [StringLength(100, ErrorMessage = "Longitud máxima 200")]
+        [Required]
         public string direccion { get; set; }
         [Display(Name = "Teléfono")]
+        [StringLength(10, ErrorMessage = "Longitud máxima 10")]
+        [Required]
         public string telefono { get; set; }
         [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Ingrese un email válido")]
+        [StringLength(100, ErrorMessage = "Longitud máxima 100")]
+        [Required]
         public string email { get; set; }
-        public DateTime fechaapertura { get; set; }
+        [Display(Name ="Fecha de apertura")]
+        [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd}", ApplyFormatInEditMode =true)]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime fechaApertura { get; set; }
+
         public int bhabilitado { get; set; }
     }
 }

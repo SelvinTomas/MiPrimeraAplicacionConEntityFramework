9fae3ad52b4bc30db32a09c9c41485ed10fed303
Selvin Tomás <tomas.a12108@gmail.com>
2022-09-01 12:02:53 -0600

    Añadiendo Formulario añadir en Cliente

--- MiPrimeraAplicacionConEntityFramework/Controllers/ClienteController.cs
+++ MiPrimeraAplicacionConEntityFramework/Controllers/ClienteController.cs
@@ -29,5 +29,49 @@ namespace MiPrimeraAplicacionConEntityFramework.Controllers
             }
             return View(listaCliente);
         }
+
+        List<SelectListItem> listaSexo;
+        private void llenarSexo()
+        {
+            using (var db = new BDPasajeEntities())
+            {
+                listaSexo = (from sexo in db.Sexo
+                             where sexo.BHABILITADO == 1
+                             select new SelectListItem
+                             {
+                                 Text=sexo.NOMBRE,
+                                 Value=sexo.IIDSEXO.ToString()
+                             }).ToList();
+            }
+        }
+        public ActionResult Agregar()
+        {
+            llenarSexo();
+            ViewBag.lista = listaSexo;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(ClienteCLS oClienteCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oClienteCLS);
+            }
+            else
+            {
+                using (var db = new BDPasajeEntities())
+                {
+                    Cliente oCliente = new Cliente();
+                    oCliente.NOMBRE = oClienteCLS.nombre;
+                    oCliente.APPATERNO = oClienteCLS.appaterno;
+                    oCliente.APMATERNO = oClienteCLS.apmaterno;
+
+                    db.Cliente.Add(oCliente);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- MiPrimeraAplicacionConEntityFramework/Models/ClienteCLS.cs
+++ MiPrimeraAplicacionConEntityFramework/Models/ClienteCLS.cs
@@ -11,18 +11,38 @@ namespace MiPrimeraAplicacionConEntityFramework.Models
         [Display(Name = "ID")]
         public int iidcliente { get; set; }
         [Display(Name = "Nombre")]
+        [Required]
+        [StringLength(100, ErrorMessage ="Longitud máxima 100")]
         public string nombre { get; set; }
         [Display(Name = "Primer Apellido")]
+        [Required]
+        [StringLength(150, ErrorMessage = "Longitud máxima 150")]
         public string appaterno { get; set; }
         [Display(Name = "Segundo Apellido")]
-    
+        [StringLength(150, ErrorMessage = "Longitud máxima 150")]
+        [Required]
         public string apmaterno { get; set; }
+        [Display(Name = "E-mail")]
+        [StringLength(200, ErrorMessage = "Longitud máxima 200")]
+        [EmailAddress(ErrorMessage ="Ingrese un Email válido")]
+        [Required]
         public string email { get; set; }
+        [Display(Name = "Dirección")]
+        [StringLength(200, ErrorMessage = "Longitud máxima 200")]
+        [Required]
         public string direccion { get; set; }
+        [Display(Name = "Sexo")]
+        [Required]
         public int iidsexo { get; set; }
         [Display(Name = "Teléfono Fijo")]
+        [StringLength(10, ErrorMessage = "Longitud máxima 10")]
+        [Required]
         public string telefonofijo { get; set; }
+        [Display(Name = "Teléfono Celular")]
+        [StringLength(10, ErrorMessage = "Longitud máxima 10")]
+        [Required]
         public string telefonocelular { get; set; }
+
         public int bhabilitado { get; set; }
 
 

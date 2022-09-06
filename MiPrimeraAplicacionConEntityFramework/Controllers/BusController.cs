658d32cf671f97e585f7909dd0543792fc3d37da
Selvin Tomás <tomas.a12108@gmail.com>
2022-09-06 10:37:20 -0600

    Listado de multiples tablas en modelo Bus

---
+++ MiPrimeraAplicacionConEntityFramework/Controllers/BusController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiPrimeraAplicacionConEntityFramework.Models;
+
+namespace MiPrimeraAplicacionConEntityFramework.Controllers
+{
+    public class BusController : Controller
+    {
+        // GET: Bus
+        public ActionResult Index()
+        {
+            List<BusCLS> listaBus = null;
+            using (var db = new BDPasajeEntities())
+            {
+                listaBus = (from bus in db.Bus
+                            join sucursal in db.Sucursal
+                            on bus.IIDSUCURSAL equals sucursal.IIDSUCURSAL
+                            join tipoBus in db.TipoBus
+                            on bus.IIDTIPOBUS equals tipoBus.IIDTIPOBUS
+                            join tipoModelo in db.Modelo
+                            on bus.IIDMODELO equals tipoModelo.IIDMODELO
+                            where bus.BHABILITADO == 1
+                            select new BusCLS
+                            {
+                                iidBus = bus.IIDBUS,
+                                nombreModelo = tipoModelo.NOMBRE,
+                                placa = bus.PLACA,
+                                nombreSucursal = sucursal.NOMBRE,
+                                nombretipoBus = tipoBus.NOMBRE
+
+                            }).ToList();
+            }
+            return View(listaBus);
+        }
+    }
+}
---
+++ MiPrimeraAplicacionConEntityFramework/Models/BusCLS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionConEntityFramework.Models
+{
+    public class BusCLS
+    {
+        [Display(Name="Id")]
+        public int iidBus { get; set; }
+        [Display(Name = "Nombre de sucursal")]
+        [Required]
+        public int iidSucursal { get; set; }
+        [Display(Name = "Tipo de bus")]
+        [Required]
+        public int iidTipoBus { get; set; }
+        [Display(Name = "Placa")]
+        [Required]
+        [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres")]
+        public string placa { get; set; }
+        [Display(Name = "Fecha de compra")]
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        public DateTime fechaCompra { get; set; }
+        [Display(Name = "Nombre de modelo")]
+        [Required]
+        public int iidModelo { get; set; }
+        [Display(Name = "Número de filas")]
+        [Required]
+        public int numeroFilas { get; set; }
+        [Display(Name = "Número de columnas")]
+        [Required]
+        public int numeroColumnas { get; set; }
+        public int bhabilitado { get; set; }
+        [Display(Name = "Descripción")]
+        [Required]
+        [StringLength(200, ErrorMessage = "Longitud máxima de 200 caracteres")]
+        public string descripcion { get; set; }
+        [Display(Name = "Observación")]
+        [StringLength(200,ErrorMessage="Longitud máxima de 200 caracteres")]
+        public string observacion { get; set; }
+
+        [Display(Name = "Marca")]
+        [Required]
+        public int iidmarca { get; set; }
+
+
+
+
+        //Propiedades adicionales
+        [Display(Name = "Nombre de Sucursal")]
+        public string nombreSucursal { get; set; }
+        [Display(Name = "Tipo de Bus")]
+        public string nombretipoBus { get; set; }
+        [Display(Name = "Modelo")]
+        public string nombreModelo { get; set; }
+
+    }
+}

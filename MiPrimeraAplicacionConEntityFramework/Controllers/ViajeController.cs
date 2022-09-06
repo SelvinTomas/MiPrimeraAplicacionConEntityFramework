6779e68d78656d07a35a60f03ef10afee48dd451
Selvin Tomás <tomas.a12108@gmail.com>
2022-09-06 11:03:25 -0600

    Añadiendo modelo y controlador Viaje

---
+++ MiPrimeraAplicacionConEntityFramework/Controllers/ViajeController.cs
@@ -0,0 +1,36 @@
+﻿using MiPrimeraAplicacionConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionConEntityFramework.Controllers
+{
+    public class ViajeController : Controller
+    {
+        // GET: Viaje
+        public ActionResult Index()
+        {
+            List<ViajeCLS> listaViaje = null;
+            using (var db = new BDPasajeEntities())
+            {
+                listaViaje = (from viaje in db.Viaje
+                              join lugarOrigen in db.Lugar
+                              on viaje.IIDLUGARORIGEN equals lugarOrigen.IIDLUGAR
+                              join lugarDestino in db.Lugar
+                              on viaje.IIDLUGARDESTINO equals lugarDestino.IIDLUGAR
+                              join bus in db.Bus
+                              on viaje.IIDBUS equals bus.IIDBUS
+                              select new ViajeCLS
+                              {
+                                  iidViaje = viaje.IIDVIAJE,
+                                  nombreBus = bus.PLACA,
+                                  nombreLugarOrigen = lugarOrigen.NOMBRE,
+                                  nombreLugarDestino = lugarDestino.NOMBRE
+                              }).ToList();
+            }
+            return View(listaViaje);
+        }
+    }
+}
---
+++ MiPrimeraAplicacionConEntityFramework/Models/ViajeCLS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionConEntityFramework.Models
+{
+    public class ViajeCLS
+    {
+        [Display(Name = "Id")]
+        public int iidViaje { get; set; }
+        [Display(Name = "Lugar de Origen")]
+        [Required]
+        public int iidLugarOrigen { get; set; }
+        [Display(Name = "Lugar de Destino")]
+        [Required]
+        public int iidLugarDestino { get; set; }
+        [Display(Name = "Precio")]
+        [Required]
+        public double precio { get; set; }
+        [Display(Name = "Fecha de viaje")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        [Required]
+        public DateTime fechaViaje { get; set; }
+        [Display(Name = "Bus")]
+        [Required]
+        public int iidBus { get; set; }
+        [Display(Name = "Numero de asientos disponibles")]
+        [Required]
+        public int  numeroAsientosDisponible { get; set; }
+
+        //Propiedades adicionales
+        [Display(Name = "Lugar de origen")]
+        public string nombreLugarOrigen { get; set; }
+        [Display(Name = "Lugar de destino")]
+        public string nombreLugarDestino { get; set; }
+        [Display(Name = "Bus")]
+        public string nombreBus { get; set; }
+
+
+
+    }
+}
